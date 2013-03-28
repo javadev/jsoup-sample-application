5ee7277ed6237a5a9deba8ec00150726508ef889
Valentyn Kolesnikov <valentyn.kolesnikov@kruschecompany.com>
2013-03-28 11:49:36 +0200

    Improved sample

--- src/main/java/com/github/jsoup/Sample.java
+++ src/main/java/com/github/jsoup/Sample.java
@@ -2,9 +2,13 @@ package com.github.jsoup;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import java.util.regex.Pattern;
 
 public class Sample {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
+        Document doc = Jsoup.connect("http://polaris.umuc.edu/~sgao/BIFS618/Homework/java.txt").get();
+System.out.println(doc.text());
+        String[] words = Pattern.compile("\\s+").split(doc.text());
+System.out.println("File has " + words.length + " words");
     }
 }
